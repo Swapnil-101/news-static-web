c60ab10bb67195721d8e42f988c64b118108d335
Swapnil-101 <swapniltiwari9819@gmail.com>
2023-05-01 13:07:33 +0530

    adad

--- components/Home/MainSection.js
+++ components/Home/MainSection.js
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from "react";
 
 const MainSection = () => {
   return (
-    <div>MainSection</div>
-  )
-}
+    <section className="flex items-center justify-center">
+      {/* <div className="flex items-center justify-center w-full h-full">
+        <h1>खबरें</h1>
+        <div className="flex w-full h-full items-center gap-2 ">
+          <div className="w-4 h-4 rounded-full bg-gray-500"></div>
+          <hr className="w-[20rem] border-2 border-gray-500 mr-4" />
+        </div>
+      </div> */}
 
-export default MainSection
+      <div className="flex gap-4">
+        <div>
+          <img
+            className="rounded w-[40rem] object-cover transition duration-500 transform hover:scale-110"
+            src="https://plus.unsplash.com/premium_photo-1664301991683-83b39364ff04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          />
+          <div>
+            <h1>
+              Rajasthan: ‘मन की बात’ कार्यक्रम में BJP सांसद बोले- राजस्थान में
+              फिर वापसी करेगी कांग्रेस, देखें वीडियो
+            </h1>
+            <h1>other data</h1>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-2">
+            <img
+              className="w-[10rem] rounded"
+              src="https://plus.unsplash.com/premium_photo-1664301991683-83b39364ff04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            />
+            <div>
+              <h1>
+                Rajasthan: ‘मन की बात’ कार्यक्रम में BJP सांसद बोले- राजस्थान
+                में फिर वापसी करेगी कांग्रेस, देखें वीडियो
+              </h1>
+              <h1>other data</h1>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <img
+              className="w-[10rem] rounded"
+              src="https://plus.unsplash.com/premium_photo-1664301991683-83b39364ff04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            />
+            <div>
+              <h1>
+                Rajasthan: ‘मन की बात’ कार्यक्रम में BJP सांसद बोले- राजस्थान
+                में फिर वापसी करेगी कांग्रेस, देखें वीडियो
+              </h1>
+              <h1>other data</h1>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <img
+              className="w-[10rem] rounded"
+              src="https://plus.unsplash.com/premium_photo-1664301991683-83b39364ff04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            />
+            <div>
+              <h1>
+                Rajasthan: ‘मन की बात’ कार्यक्रम में BJP सांसद बोले- राजस्थान
+                में फिर वापसी करेगी कांग्रेस, देखें वीडियो
+              </h1>
+              <h1>other data</h1>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <img
+              className="w-[10rem] rounded"
+              src="https://plus.unsplash.com/premium_photo-1664301991683-83b39364ff04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            />
+            <div>
+              <h1>
+                Rajasthan: ‘मन की बात’ कार्यक्रम में BJP सांसद बोले- राजस्थान
+                में फिर वापसी करेगी कांग्रेस, देखें वीडियो
+              </h1>
+              <h1>other data</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainSection;
